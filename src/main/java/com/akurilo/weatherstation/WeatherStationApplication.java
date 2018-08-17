6ba0d49db309476ecc00f56192529c920ce7ab7c
aleksk <aleks_prime@mail.ru>
2018-08-17 08:04:52 +0300

    start project

---
+++ src/main/java/com/akurilo/weatherstation/WeatherStationApplication.java
@@ -0,0 +1,12 @@
+package com.akurilo.weatherstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherStationApplication.class, args);
+    }
+}
